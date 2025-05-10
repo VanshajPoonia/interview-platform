2a0e6a2849407abe1fdcd8b1717eb6bc6c524ff7
Vanshaj Poonia <74147463+VanshajPoonia@users.noreply.github.com>
2025-05-10 10:41:03 +0000

    updated api values

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,14 +8,23 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDtgNUilYhvGj8vMOhnrzbzWAcuDRNCQh4",
+  authDomain: "interview-prep-f0d86.firebaseapp.com",
+  projectId: "interview-prep-f0d86",
+  storageBucket: "interview-prep-f0d86.firebasestorage.app",
+  messagingSenderId: "402926383626",
+  appId: "1:402926383626:web:edfa45c01733a1a30aa5f3",
+  measurementId: "G-E3WTR3PDZT"
 };
 
 // Initialize Firebase
